9e42afb2c4ae4ed34af451139efa17f5f32b892c
LorisPanaroGit <lorispanaro28@gmail.com>
2025-01-28 00:08:31 +0100

    Created helper function for passing data among test functions

--- tests/qtest/stm32f2xx_gpio-test.c
+++ tests/qtest/stm32f2xx_gpio-test.c
@@ -1,3 +1,7 @@
+/*
+meson test qtest-arm/stm32f2xx_gpio-test -> run single meson test for GPIO
+*/
+
 #include "qemu/osdep.h"
 #include "libqtest-single.h"
 
@@ -21,6 +25,23 @@
 #define GPIOx_BRR  0x14
 #define GPIOx_LCKR 0x18
 
+#define GPIO_PIN_0  0
+#define GPIO_PIN_1  1
+#define GPIO_PIN_2  2
+#define GPIO_PIN_3  3
+#define GPIO_PIN_4  4
+#define GPIO_PIN_5  5
+#define GPIO_PIN_6  6
+#define GPIO_PIN_7  7
+#define GPIO_PIN_8  8
+#define GPIO_PIN_9  9
+#define GPIO_PIN_10 10
+#define GPIO_PIN_11 11
+#define GPIO_PIN_12 12
+#define GPIO_PIN_13 13
+#define GPIO_PIN_14 14
+#define GPIO_PIN_15 15
+
 #define RESET_CRL  0x44444444
 #define RESET_CRH  0x44444444
 #define RESET_IDR  0x00000000
@@ -32,6 +53,12 @@
 /* SoC forwards GPIOs to SysCfg */
 #define SYSCFG "/machine/soc"
 
+#define PIN_MASK        0xF
+#define GPIO_ADDR_MASK  (~(GPIO_PORT_SIZE - 1))
+
+#define GPIO_ADDR(data) ((uintptr_t)(data) & GPIO_ADDR_MASK)
+#define GPIO_PIN(data) ((uintptr_t)(data) & PIN_MASK)
+
 unsigned int reset_vals[GPIO_NUM_REGS] = {
     RESET_CRL,
     RESET_CRH,
@@ -41,6 +68,11 @@ unsigned int reset_vals[GPIO_NUM_REGS] = {
     RESET_BRR,
     RESET_LCKR
 };
+
+/*test_data() just builds a number by taking the offset of the GPIOx_line and concatenates the pin number*/
+static inline void* test_data(uint32_t gpio_line, uint8_t pin_num) {
+    return (void*)(uintptr_t) ((gpio_line & GPIO_ADDR_MASK) | (pin_num & PIN_MASK));
+}
 
 /*Helper function for writel: pass the GPIO_X group for offset (register address) computing*/
 static void gpio_writel(unsigned int gpio_port, unsigned int reg, uint32_t val) {
@@ -85,25 +117,28 @@ static void stm32f2xx_test_reset_values(void) {
 
 /*static void stm32f2xx_test_gpio_output_mode(const void *data) {
 
-}
+}*/
 
 static void stm32f2xx_test_gpio_input_mode(const void *data) {
-
+    //uint32_t pin_obj = GPIO_PIN(data);
+    //uint32_t gpio_addr = GPIO_ADDR(data);
+    //qtest_irq_intercept_in(global_qtest, SYSCFG);
+    /*Set GPIOx[GPIO_PIN_<number>] */
 }
 
-static void stm32f2xx_test_pull_up_pull_down(const void *data) {
+/*static void stm32f2xx_test_pull_up_pull_down(const void *data) {
 
-}
+}*/
 
-static void stm32f2xx_test_push_pull(const void *data) {
+/*static void stm32f2xx_test_push_pull(const void *data) {
 
-}
+}*/
 
-static void stm32f2xx_test_open_drain(const void *data) {
+/*static void stm32f2xx_test_open_drain(const void *data) {
 
-}
+}*/
 
-static void stm32f2xx_test_bsrr_brr(const void *data) {
+/*static void stm32f2xx_test_bsrr_brr(const void *data) {
 
 }*/
 
@@ -112,6 +147,7 @@ int main(int argc, char **argv) {
     g_test_init(&argc, &argv, NULL);
     g_test_set_nonfatal_assertions();
     qtest_add_func("stm32f2xx/gpio/stm32f2xx_test_reset_values", stm32f2xx_test_reset_values);
+    qtest_add_data_func("stm32f2xx/gpio/stm32f2xx_test_input_mode_gpioa", test_data(GPIO_A_ADDR, GPIO_PIN_0), stm32f2xx_test_gpio_input_mode);
     qtest_start("-machine stm32vldiscovery");
     ret = g_test_run();
     qtest_end();
